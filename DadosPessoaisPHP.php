bdc3224f94a65758c7fb08e9268ffd55c45777bc
Starjed <biel123albuquerque@hotmail.com>
2022-03-18 21:29:28 -0400

    DadosPessoaisPhP

---
+++ DadosPessoaisPHP.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Dados Pessoais PHP</h1>
+    <?php
+      echo "Nome: Jonathan Soares";
+      echo "Data Nasc. 00/00/0000";
+      echo "Cidade: Jamayca";
+      echo "Estado: Espigão do Oeste";
+      echo "Endereço: Logo ali depois do fim a esquerda, N° 9999";
+    ?>
+  </body>
+</html>
